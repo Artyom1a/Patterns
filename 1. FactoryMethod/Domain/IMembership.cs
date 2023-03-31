38cbb48867f37047b2614b4befd6bccc64fbbd4e
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-31 14:17:43 +0300

    31.03Patterns

---
+++ 1. FactoryMethod/Domain/IMembership.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessClub.Domain
+{
+    internal interface IMembership //интерфейс абонемента
+    {
+        string Name { get; }
+        string Description { get; set; }
+        decimal GetPrice(); //метод цены
+    }
+}
