ab36f9b614725de1e001d2d5fa22babe68e39cdf
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-01 13:03:45 +0300

    010423

---
+++ 1. FactoryMethod/Factories/GymMembershipFactory.cs
@@ -0,0 +1,26 @@
+﻿using FitnessClub.Domain;
+
+namespace FitnessClub.Factories
+{
+    internal class GymMembershipFactory : MembershipFactory
+    {
+        private readonly decimal _price;
+        private readonly string _description;
+
+        public GymMembershipFactory(decimal price, string description)
+        {
+            _price = price;
+            _description = description;
+        }
+
+        public override IMembership GetMembership() // реализация фабричного метода (мы его переопределяем)
+        {
+            GymMembership membership = new(_price) // создаём абонемент только для посещения спортзала
+            {
+                Description = _description
+            };
+
+            return membership;
+        }
+    }
+}
