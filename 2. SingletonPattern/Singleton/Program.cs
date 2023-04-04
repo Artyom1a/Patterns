e2e47963d4991f2996f9dc88ea5c81eb33610ebd
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-04 12:55:27 +0300

    feature0404n2

---
+++ 2. SingletonPattern/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using Singleton;
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        // однопоточность
+        //Computer comp = new Computer();
+        //comp.Launch("Windows 8.1");
+        //Console.WriteLine(comp.OS.Name);
+
+        //// у нас не получится изменить ОС, так как объект уже создан    
+        //comp.OS = OS.getInstance("Windows 10");
+        //Console.WriteLine(comp.OS.Name);
+
+        //Console.ReadLine();
+        // однопоточность
+
+        // многопоточность
+        (new Thread(() =>
+        {
+            Computer comp2 = new Computer();
+            comp2.OS = OS.getInstance("Windows 10");
+            Console.WriteLine(comp2.OS.Name);
+
+        })).Start();
+
+        Computer comp = new Computer();
+        comp.Launch("Windows 8.1");
+        Console.WriteLine(comp.OS.Name);
+        Console.ReadLine();
+        // многопоточность
+    }
+}
+
